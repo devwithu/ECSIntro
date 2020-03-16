8768de4cc2ac4be664b1b0cdd82673d2bfb5ca1d
devwithu <devwithu@naver.com>
2020-03-17 07:50:24 +0900

    init

---
+++ Assets/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Mesh unitMesh;
+
+    [SerializeField] private Material unitMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeEntity();
+    }
+
+    private void MakeEntity()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        EntityArchetype archetype = entityManager.CreateArchetype(
+            typeof(Transform),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+        Entity myEntity =  entityManager.CreateEntity(archetype);
+        entityManager.AddComponentData(myEntity, new Translation
+        {
+            Value = new float3(2f, 0f, 4f)
+        });
+        entityManager.AddSharedComponentData(myEntity, new RenderMesh
+        {
+            mesh = unitMesh,
+            material = unitMaterial
+        });
+    }
+}
