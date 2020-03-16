6ec3ce7253c4926f95f324acb7838c82b33665c4
devwithu <devwithu@naver.com>
2020-03-17 08:17:57 +0900

    Conversion Workflow

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,18 +4,61 @@ using Unity.Transforms;
 using Unity.Rendering;
 using Unity.Mathematics;
 
+
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Mesh unitMesh;
 
     [SerializeField] private Material unitMaterial;
+
+    [SerializeField] private GameObject gameObjectPrefab;
+
+    [SerializeField] private int xSize = 10;
+    [SerializeField] private int ySize = 10;
+    [Range(0.1f, 2f)]
+    [SerializeField] private float spacing = 1f;
+    
+    private Entity entityPrefab;
+
+    private World defaultWorld;
+
+    private EntityManager entityManager;
     
     // Start is called before the first frame update
     void Start()
     {
-        MakeEntity();
+        //MakeEntity();
+        defaultWorld = World.DefaultGameObjectInjectionWorld;
+        entityManager = defaultWorld.EntityManager;
+
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(defaultWorld, null);
+        entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(gameObjectPrefab, settings);
+        
+        //InstantiateEntity(new float3(4f, 0f, 4f));
+        InstantiateEntityGrid(xSize,ySize,spacing);
+        
     }
 
+    private void InstantiateEntity(float3 positioin)
+    {
+        Entity myEntity = entityManager.Instantiate(entityPrefab);
+        entityManager.SetComponentData(myEntity, new Translation
+        {
+            Value = positioin
+        });
+    }
+
+    private void InstantiateEntityGrid(int dimX, int dimY, float spacing = 1f)
+    {
+        for (int i = 0; i < dimX; i++)
+        {
+            for (int j = 0; j < dimY; j++)
+            {
+                InstantiateEntity(new float3(i * spacing, j * spacing, 0f)  );
+                
+            }
+        }
+    }
     private void MakeEntity()
     {
         EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
